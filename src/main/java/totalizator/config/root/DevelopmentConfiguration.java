bed2cdfc9845733b5a77a91114cbab479730d12a
blu <blu@encode.dk>
2015-02-16 17:16:39 +0200

    Project Structure 10

--- src/main/java/totalizator/config/root/DevelopmentConfiguration.java
+++ src/main/java/totalizator/config/root/DevelopmentConfiguration.java
@@ -1,13 +1,48 @@
 package totalizator.config.root;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @Profile( "development" )
 @EnableTransactionManagement
 public class DevelopmentConfiguration {
 
+	/*@Bean(name = "datasource")
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName( "org.postgresql.Driver" );
+		dataSource.setUrl( "jdbc:postgresql://localhost:5432/calorie-tracker?loglevel=0" );
+		dataSource.setUsername( "postgres" );
+		dataSource.setPassword( "postgres" );
+		return dataSource;
+	}*/
 
+	/*@Bean(name = "entityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory( DriverManagerDataSource dataSource ) {
+
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setDataSource( dataSource );
+		entityManagerFactoryBean.setPackagesToScan( new String[]{ "calories.tracker.app.model" } );
+		entityManagerFactoryBean.setLoadTimeWeaver( new InstrumentationLoadTimeWeaver() );
+		entityManagerFactoryBean.setJpaVendorAdapter( new HibernateJpaVendorAdapter() );
+
+		Map<String, Object> jpaProperties = new HashMap<String, Object>();
+		jpaProperties.put( "hibernate.hbm2ddl.auto", "create-drop" );
+		jpaProperties.put( "hibernate.show_sql", "true" );
+		jpaProperties.put( "hibernate.format_sql", "true" );
+		jpaProperties.put( "hibernate.use_sql_comments", "true" );
+		entityManagerFactoryBean.setJpaPropertyMap( jpaProperties );
+
+		return entityManagerFactoryBean;
+	}*/
 }
