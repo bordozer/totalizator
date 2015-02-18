2c8fe3cb7db3e57327e27dead14f9fa44200d473
blu <blu@encode.dk>
2015-02-18 10:17:07 +0200

    Optiimport

--- src/main/java/totalizator/app/controllers/login/LoginPageController.java
+++ src/main/java/totalizator/app/controllers/login/LoginPageController.java
@@ -1,9 +1,11 @@
 package totalizator.app.controllers.login;
 
 import org.apache.log4j.Logger;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
