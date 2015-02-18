a2ecaf740734777d4114b038010c5fc35822a1a5
blu <blu@encode.dk>
2015-02-18 16:13:35 +0200

    Persistance 3

--- src/main/java/totalizator/app/security/SecurityUserDetailsService.java
+++ src/main/java/totalizator/app/security/SecurityUserDetailsService.java
@@ -1,12 +1,15 @@
 package totalizator.app.security;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import totalizator.app.dao.UserRepository;
+import totalizator.app.models.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,8 +19,19 @@ public class SecurityUserDetailsService implements UserDetailsService {
 
 	private static final Logger LOGGER = Logger.getLogger( SecurityUserDetailsService.class );
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Override
 	public UserDetails loadUserByUsername( final String username ) throws UsernameNotFoundException {
+
+		final User user = userRepository.findUserByName( username );
+
+		if ( user == null ) {
+			String message = "Username not found" + username;
+			LOGGER.info( message );
+			throw new UsernameNotFoundException( message );
+		}
 
 		final List<GrantedAuthority> authorities = new ArrayList<>();
 		authorities.add( new SimpleGrantedAuthority( "ROLE_USER" ) );
