96ca4c3698306d74a6413a458cb9040f3e502579
blu <blu@encode.dk>
2015-02-19 11:26:53 +0200

    Default users - test data

--- src/main/java/totalizator/app/init/TestDataInitializer.java
+++ src/main/java/totalizator/app/init/TestDataInitializer.java
@@ -31,8 +31,14 @@ public class TestDataInitializer {
 			session.persist( user );
 		}*/
 
-		final User user = new User( "login_1", "name_1", "$2a$10$x9vXeDsSC2109FZfIJz.pOZ4dJ056xBpbesuMJg3jZ.ThQkV119tS" );
-		session.persist( user );
+		final User user1 = new User( "kareem", "Kareem Abdul Jabbar", "$2a$10$11.wSzimtc3ALjbjokULr.LLJb4A/iDh3P6rw0..nI9JcpTpwN6M6" );
+		session.persist( user1 );
+
+		final User user2 = new User( "hakeem", "Hakeem Olajuwon", "$2a$10$VNleCLXwo0TNYCcb4m1rWOjx2SU1qs6hyav04Ip8Zq0eZu3/Al1aS" );
+		session.persist( user2 );
+
+		final User user3 = new User( "patrick", "Patrick Aloysius Ewing", "$2a$10$daF6I2MzfIcmGg/pWNmc4ep8OLkjbDDRxRhnKhMpbTOyLNacpcMVO" );
+		session.persist( user3 );
 
 		transaction.commit();
 
