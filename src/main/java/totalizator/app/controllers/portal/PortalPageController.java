db087f41d27ccc1e7ac880ed2a7df4e8fe8e2641
blu <blu@encode.dk>
2015-02-19 12:33:29 +0200

    Portal page controller 2

--- src/main/java/totalizator/app/controllers/portal/PortalPageController.java
+++ src/main/java/totalizator/app/controllers/portal/PortalPageController.java
@@ -4,7 +4,10 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import totalizator.app.models.User;
 import totalizator.app.services.UserService;
 
@@ -22,10 +25,11 @@ public class PortalPageController {
 	@ResponseStatus( HttpStatus.OK )
 	@ResponseBody
 	@RequestMapping( method = RequestMethod.GET, value = "/", produces = APPLICATION_JSON_VALUE )
-	public PortalPageDTO getDefaultLogin( final @RequestBody PortalPageDTO portalPageDTO ) {
+	public PortalPageDTO getDefaultLogin() {
 
 		final User user = userService.findUserByLogin( "hakeem" );
 
+		final PortalPageDTO portalPageDTO = new PortalPageDTO();
 		portalPageDTO.setUserName( user.getUsername() );
 
 		return portalPageDTO;
