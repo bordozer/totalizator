671eddfe248cfc6e8143b97fae4df2dff247b331
blu <blu@encode.dk>
2015-02-27 15:54:32 +0200

    Teams: 7: filtering by category 3

--- db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `UUID` varchar(255) DEFAULT NULL,
-  `version` bigint(20) DEFAULT NULL,
-  `login` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
