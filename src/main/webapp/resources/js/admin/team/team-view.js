01ff743920b1d438a0cd3c057535d9a7cf0775bd
blu <blu@encode.dk>
2015-02-27 13:40:20 +0200

    Teams: 2 translations

--- src/main/webapp/resources/js/admin/team/team-view.js
+++ src/main/webapp/resources/js/admin/team/team-view.js
@@ -8,6 +8,11 @@ define( function ( require ) {
 
 	var Template = require( 'text!js/admin/team/templates/team-template.html' );
 
+	var Translator = require( 'translator' );
+	var translator = new Translator( {
+		teamTitleLabel: "Admin / Teams / Title: Teams"
+	} );
+
 	var TeamsView = Backbone.View.extend( {
 
 		template: _.template( Template ),
@@ -18,6 +23,7 @@ define( function ( require ) {
 
 		render: function() {
 			this.$el.html( this.template( {
+				translator: translator
 			 } ) );
 
 			return this;
