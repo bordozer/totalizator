04a769248bb4798de4d12cdb4205c55a18b31b9d
bordozer <bordozer@gmail.com>
2015-03-31 09:58:08 +0300

    ...

--- src/main/webapp/resources/js/base/base-page-view.js
+++ src/main/webapp/resources/js/base/base-page-view.js
@@ -49,7 +49,7 @@ define( function ( require ) {
 		},
 
 		_renderMenu: function() {
-			mainMenu( this.mainMenuItems(), 'fa-list-alt', this.$( '.main-menu-container') );
+			mainMenu( this.mainMenuItems(), 'fa-list-alt', this.$( '.js-main-menu-container') );
 		},
 
 		_getPageSubTitle: function() {
