26dc7b74bc93f1ac4b8526533b84bf78c85fd283
blu <blu@encode.dk>
2015-04-02 11:30:14 +0300

    Packages arrangment

--- src/main/webapp/resources/js/matches/matches-view.js
+++ src/main/webapp/resources/js/matches/matches-view.js
@@ -8,8 +8,8 @@ define( function ( require ) {
 
 	var ConfigurableView = require( 'js/components/configurable-view/configurable-view' );
 
-	var template = _.template( require( 'text!js/matches/templates/matches-template.html' ) );
-	var templateMatch = _.template( require( 'text!js/matches/templates/match-template.html' ) );
+	var template = _.template( require( 'text!./templates/matches-template.html' ) );
+	var templateMatch = _.template( require( 'text!./templates/match-template.html' ) );
 
 	var dateTimeService = require( '/resources/js/dateTimeService.js' );
 	var service = require( '/resources/js/services.js' );
