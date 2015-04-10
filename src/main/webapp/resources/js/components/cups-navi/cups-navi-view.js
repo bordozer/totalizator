a3ee19e5025dadb3f8983ad5fcc74e22d02f6908
blu <blu@encode.dk>
2015-04-10 16:42:52 +0300

    Set active tab 2

--- src/main/webapp/resources/js/components/cups-navi/cups-navi-view.js
+++ src/main/webapp/resources/js/components/cups-navi/cups-navi-view.js
@@ -11,7 +11,6 @@ define( function ( require ) {
 	return Backbone.View.extend( {
 
 		initialize: function( options ) {
-			this.selectedCupId = options.selectedCupId;
 
 			this.on( 'navigation:set:active:cup', this._setSelectedCupId, this );
 
