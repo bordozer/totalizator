f6dc14ab6b06b8ea0373448b229085763cd6b2c3
blu <blu@encode.dk>
2015-04-10 15:58:14 +0300

    bugs fixing 5

--- src/main/webapp/resources/js/pages/user-list/user-list-view.js
+++ src/main/webapp/resources/js/pages/user-list/user-list-view.js
@@ -18,6 +18,7 @@ define( function ( require ) {
 	return Backbone.View.extend( {
 
 		initialize: function( options ) {
+			this.currentUser = options.options.currentUser;
 			this.model.on( 'sync', this.render, this );
 			this.model.fetch( { cache: false } );
 		},
@@ -26,30 +27,16 @@ define( function ( require ) {
 
 			this.$el.html( template( {
 				users: this.model.toJSON()
+				, currentUser: this.currentUser
 				, translator: translator
 			} ) );
 
 			this._renderNavigation();
-
-//			this._renderUserList();
 		},
 
 		_renderNavigation: function() {
 			var selectedCupId = 0;
 			var cupsNaviView = new CupsNaviView( selectedCupId, this.$( '.js-cups-navi' ) );
 		}
-
-		/*_renderUserList: function() {
-			var el = this.$( '.js-user-list' );
-
-			var userRenderer = _.bind( this._userRenderer, this );
-			this.model.forEach( userRenderer );
-		},
-
-		_userRenderer: function( model ) {
-			var user = model.toJSON();
-
-			this.$( '.js-user-list' ).append( user.userName );
-		}*/
 	});
 } );
