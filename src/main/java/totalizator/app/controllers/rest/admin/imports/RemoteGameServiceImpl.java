f1013aedcfb1b8def675155a12dd9b3b028463c1
bordozer <bordozer@gmail.com>
2015-04-16 15:41:55 +0300

    NBA import - 11 parse json 2

--- src/main/java/totalizator/app/controllers/rest/admin/imports/RemoteGameServiceImpl.java
+++ src/main/java/totalizator/app/controllers/rest/admin/imports/RemoteGameServiceImpl.java
@@ -1,17 +1,50 @@
 package totalizator.app.controllers.rest.admin.imports;
 
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import totalizator.app.controllers.rest.admin.imports.nba.NBAGame;
+import totalizator.app.services.utils.DateTimeService;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Service
 public class RemoteGameServiceImpl implements RemoteGameService {
 
+	@Autowired
+	private DateTimeService dateTimeService;
+
 	@Override
 	public RemoteGame parseGame( final String remoteGameJSON ) {
 
-		final NBAGame nbaGame = new Gson().fromJson( remoteGameJSON, NBAGame.class );
+		final Gson gson = new Gson();
+
+
+		final NBAGame nbaGame = gson.fromJson( remoteGameJSON, NBAGame.class );
+
+		final LocalDateTime gameTime = getDate( nbaGame );
+
+		final String _team1 = ( String ) ( ( ArrayList ) ( ( ArrayList ) nbaGame.getResultSets().get( 3 ).get( "rowSet" ) ).get( 0 ) ).get( 5 );
+		final int score1 = ( Integer ) ( ( ArrayList ) ( ( ArrayList ) nbaGame.getResultSets().get( 3 ).get( "rowSet" ) ).get( 0 ) ).get( 7 );
+
+		final String _team2 = ( String ) ( ( ArrayList ) ( ( ArrayList ) nbaGame.getResultSets().get( 3 ).get( "rowSet" ) ).get( 0 ) ).get( 10 );
+		final int score2 = ( Integer ) ( ( ArrayList ) ( ( ArrayList ) nbaGame.getResultSets().get( 3 ).get( "rowSet" ) ).get( 0 ) ).get( 12 );
+
+		final boolean isFinal = ( ( String ) ( ( ArrayList ) ( ( ArrayList ) nbaGame.getResultSets().get( 0 ).get( "rowSet" ) ).get( 0 ) ).get( 4 ) ).equals( "Final" );
+
+		final String homeTeamId = ( String ) ( ( ArrayList ) ( ( ArrayList ) nbaGame.getResultSets().get( 0 ).get( "rowSet" ) ).get( 0 ) ).get( 6 );
+		final String team1TeamId = ( String ) ( ( ArrayList ) ( ( ArrayList ) nbaGame.getResultSets().get( 3 ).get( "rowSet" ) ).get( 0 ) ).get( 3 );
+
+		final String _homeTeam = team1TeamId.equals( homeTeamId ) ? _team1 : _team2;
+
 
 		return new RemoteGame();
+	}
+
+	private LocalDateTime getDate( final NBAGame nbaGame ) {
+		final String _date = ( String ) ( ( ArrayList ) ( ( ArrayList ) nbaGame.getResultSets().get( 0 ).get( "rowSet" ) ).get( 0 ) ).get( 0 );
+
+		return LocalDateTime.parse( _date );
 	}
 }
--- src/main/java/totalizator/app/controllers/rest/admin/imports/nba/NBAGame.java
+++ src/main/java/totalizator/app/controllers/rest/admin/imports/nba/NBAGame.java
@@ -8,7 +8,27 @@ import java.util.List;
 @JsonIgnoreProperties( ignoreUnknown = true )
 public class NBAGame {
 
-	private List<LinkedTreeMap<Object, NBAResultSetEntry>> resultSets;
+	private List<LinkedTreeMap<Object, Object>> resultSets;
+
+	public List<LinkedTreeMap<Object, Object>> getResultSets() {
+		return resultSets;
+	}
+
+	public void setResultSets( final List<LinkedTreeMap<Object, Object>> resultSets ) {
+		this.resultSets = resultSets;
+	}
+
+	/*private List<NBAResultSetEntry> resultSets;
+
+	public List<NBAResultSetEntry> getResultSets() {
+		return resultSets;
+	}
+
+	public void setResultSets( final List<NBAResultSetEntry> resultSets ) {
+		this.resultSets = resultSets;
+	}*/
+
+	/*private List<LinkedTreeMap<Object, NBAResultSetEntry>> resultSets;
 
 	public List<LinkedTreeMap<Object, NBAResultSetEntry>> getResultSets() {
 		return resultSets;
@@ -16,5 +36,5 @@ public class NBAGame {
 
 	public void setResultSets( final List<LinkedTreeMap<Object, NBAResultSetEntry>> resultSets ) {
 		this.resultSets = resultSets;
-	}
+	}*/
 }
