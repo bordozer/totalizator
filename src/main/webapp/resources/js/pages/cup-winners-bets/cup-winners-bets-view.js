647545a32379ede465ab0e8db4928ed59e2718a0
bordozer <bordozer@gmail.com>
2015-04-19 10:17:37 +0300

    allCupBets 3

--- src/main/webapp/resources/js/pages/cup-winners-bets/cup-winners-bets-view.js
+++ src/main/webapp/resources/js/pages/cup-winners-bets/cup-winners-bets-view.js
@@ -6,7 +6,7 @@ define( function ( require ) {
 	var _ = require( 'underscore' );
 	var $ = require( 'jquery' );
 
-	var template = _.template( require( 'text!./templates/' ) );
+	var template = _.template( require( 'text!./templates/cup-winners-bets-template.html' ) );
 
 	var Translator = require( 'translator' );
 	var translator = new Translator( {
@@ -20,10 +20,14 @@ define( function ( require ) {
 		},
 
 		initialize: function ( options ) {
+			this.cupId = options.options.cupId;
+
 			this.render();
 		},
 
 		render: function () {
+
+			this.trigger( 'navigation:set:active:cup', { selectedCupId: this.cupId } );
 
 			var data = _.extend( {}, this.model.toJSON(), { translator: translator } );
 
