0b11761e1ed4957db9ab952dbb8839f0512ad859
bordozer <bordozer@gmail.com>
2015-04-19 11:45:13 +0300

    allCupBets 11

--- src/main/webapp/resources/js/pages/cup-winners-bets/cup-winners-bets-view.js
+++ src/main/webapp/resources/js/pages/cup-winners-bets/cup-winners-bets-view.js
@@ -31,7 +31,6 @@ define( function ( require ) {
 		render: function () {
 
 			var model = this.model.toJSON();
-			console.log( model );
 
 			this.trigger( 'navigation:set:active:cup', { selectedCupId: this.cupId } );
 
