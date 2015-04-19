630e749fada25a9a25fc87d3cd4c2fe5919257d0
bordozer <bordozer@gmail.com>
2015-04-19 13:29:06 +0300

    ..

--- src/main/java/totalizator/config/WebAppInitializer.java
+++ src/main/java/totalizator/config/WebAppInitializer.java
@@ -1,6 +1,7 @@
 package totalizator.config;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import totalizator.config.root.DevelopmentConfiguration;
 import totalizator.config.root.RootContextConfig;
