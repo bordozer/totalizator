94f0fb202c15a20785d977b55aaed8c10fffb1fb
blu <blu@encode.dk>
2015-04-30 14:16:08 +0300

    Teams stand off - optimizing

--- src/main/java/totalizator/app/controllers/rest/teams/standoffs/TeamsStandoffsRestController.java
+++ src/main/java/totalizator/app/controllers/rest/teams/standoffs/TeamsStandoffsRestController.java
@@ -1,22 +1,23 @@
 package totalizator.app.controllers.rest.teams.standoffs;
 
-import org.apache.commons.collections15.CollectionUtils;
-import org.apache.commons.collections15.Predicate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import totalizator.app.models.Category;
 import totalizator.app.models.Cup;
+import totalizator.app.models.Match;
 import totalizator.app.models.Team;
-import totalizator.app.services.CupService;
 import totalizator.app.services.DTOService;
+import totalizator.app.services.MatchService;
 import totalizator.app.services.TeamService;
 import totalizator.app.services.UserService;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Set;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newLinkedHashSet;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Controller
@@ -24,13 +25,13 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class TeamsStandoffsRestController {
 
 	@Autowired
-	private CupService cupService;
-
-	@Autowired
 	private TeamService teamService;
 
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private MatchService matchService;
 
 	@Autowired
 	private DTOService dtoService;
@@ -40,17 +41,15 @@ public class TeamsStandoffsRestController {
 	@RequestMapping( method = RequestMethod.GET, value = "{team1Id}/vs/{team2Id}/", produces = APPLICATION_JSON_VALUE )
 	public TeamsStandoffsDTO all( final @PathVariable( "team1Id" ) int team1Id, final @PathVariable( "team2Id" ) int team2Id, final Principal principal ) {
 
-		final Team team = teamService.load( team1Id );
-		final Category category = team.getCategory();
+		final Team team1 = teamService.load( team1Id );
+		final Team team2 = teamService.load( team2Id );
 
-		final List<Cup> cups = cupService.loadAllPublic();
-		CollectionUtils.filter( cups, new Predicate<Cup>() {
-			@Override
-			public boolean evaluate( final Cup cup ) {
-				return cup.getCategory().equals( category );
-			}
-		} );
+		final Set<Cup> cups = newLinkedHashSet();
+		final List<Match> matches = matchService.find( team1, team2 );
+		for ( final Match match : matches ) {
+			cups.add( match.getCup() );
+		}
 
-		return new TeamsStandoffsDTO( dtoService.transformCups( cups, userService.findByLogin( principal.getName() ) ) );
+		return new TeamsStandoffsDTO( dtoService.transformCups( newArrayList( cups ), userService.findByLogin( principal.getName() ) ) );
 	}
 }
