394798a40d488a03e72e65aa06eddb09d7aefc6c
blu <blu@encode.dk>
2015-05-08 15:43:01 +0300

    Cache Level II: 42.1

--- src/main/java/totalizator/config/root/RootContextConfig.java
+++ src/main/java/totalizator/config/root/RootContextConfig.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 		, "totalizator.app.security"
 		, "totalizator.app.translator"
 } )
-//@EnableCaching
 public class RootContextConfig {
 
 	@Bean( name = "transactionManager" )
