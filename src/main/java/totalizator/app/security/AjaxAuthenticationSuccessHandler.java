75ecea07af537adc2df610a7b2059c6d61eba5b7
blu <blu@encode.dk>
2015-07-10 17:15:03 +0300

    Language 2

--- src/main/java/totalizator/app/security/AjaxAuthenticationSuccessHandler.java
+++ src/main/java/totalizator/app/security/AjaxAuthenticationSuccessHandler.java
@@ -4,15 +4,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import totalizator.app.beans.AppContext;
 import totalizator.app.translator.Language;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Component
 public class AjaxAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+	public static final String APPLICATION_CONTEXT = "APPLICATION_CONTEXT";
 
 	private AuthenticationSuccessHandler defaultHandler;
 
@@ -26,6 +30,12 @@ public class AjaxAuthenticationSuccessHandler implements AuthenticationSuccessHa
 		if ( "true".equals( request.getHeader( "X-Login-Ajax-call" ) ) ) {
 
 			final Language language = Language.getByCode( request.getParameter( "language" ) );
+
+			final AppContext context = new AppContext();
+			context.setLanguage( language );
+
+			final HttpSession session = request.getSession();
+			session.setAttribute( APPLICATION_CONTEXT, context );
 
 			response.getWriter().print( "ok" );
 			response.getWriter().flush();
