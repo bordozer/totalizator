1480e37071e2ae21374213ce62ff2cab4afd5399
blu <blu@encode.dk>
2015-08-18 13:38:21 +0300

    travis autodeploy IV

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "$(pwd)"
-chmod 600 ./id_rsa
-mv ./id_rsa ~/.ssh/id_rsa
+chmod 600 ./travis/id_rsa
+mv ./travis/id_rsa ~/.ssh/id_rsa
 echo "Host *" >> ~/.ssh/config
 echo "  BatchMode yes" >> ~/.ssh/config
 ssh totalizator@encoders.kiev.ua /home/totalizator/update.sh
