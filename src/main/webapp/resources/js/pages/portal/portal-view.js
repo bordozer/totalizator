0063c7a93c331674aecf740a55b69073ef25f7e4
bordozer <bordozer@gmail.com>
2015-10-05 19:00:48 +0300

    Postal - dates control progress

--- src/main/webapp/resources/js/pages/portal/portal-view.js
+++ src/main/webapp/resources/js/pages/portal/portal-view.js
@@ -130,6 +130,8 @@ define( function ( require ) {
 
 			this.model.previousDay();
 
+			this.spinning();
+
 			this.model.refresh();
 		},
 
@@ -137,7 +139,14 @@ define( function ( require ) {
 
 			this.model.nextDay();
 
+			this.spinning();
+
 			this.model.refresh();
+		},
+
+		spinning: function () {
+			this.$( '.js-portal-page-matches-on-date' ).html( "<div class='text-center'><i class='fa fa-spinner fa-spin fa-5x'></i></div>" );
+			this.$( '.js-portal-page-container' ).html( "<div class='text-center'><i class='fa fa-spinner fa-spin fa-5x'></i></div>" );
 		}
 	} );
 } );
