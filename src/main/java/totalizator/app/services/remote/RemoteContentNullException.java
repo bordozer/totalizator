cc95f065a527ec963466edf5c8a7e7f15a5d790b
Borys Lukianov <Borys_Lukianov@epam.com>
2016-02-27 18:47:33 +0200

    NBA import hack fix

--- src/main/java/totalizator/app/services/remote/RemoteContentNullException.java
+++ src/main/java/totalizator/app/services/remote/RemoteContentNullException.java
@@ -4,9 +4,16 @@ import org.apache.log4j.Logger;
 
 public class RemoteContentNullException extends Exception {
 
-	private static final Logger LOGGER = Logger.getLogger( RemoteContentNullException.class );
+    private static final Logger LOGGER = Logger.getLogger(RemoteContentNullException.class);
 
-	public RemoteContentNullException( final String url ) {
-		LOGGER.error( url );
-	}
+    private String url;
+
+    public RemoteContentNullException(final String url) {
+        LOGGER.error(url);
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
