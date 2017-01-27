e26b274296517735f95847248f7b7fb57121e68f
Borys Lukianov <Borys_Lukianov@epam.com>
2017-01-27 12:35:51 +0200

    ...

--- betmen-web/src/main/webapp/resources/js/widgets/fav-cat-stat/fav-cat-stat-widget-view.js
+++ betmen-web/src/main/webapp/resources/js/widgets/fav-cat-stat/fav-cat-stat-widget-view.js
@@ -8,12 +8,12 @@ define(function (require) {
 
     var template = _.template(require('text!./templates/fav-cat-stat-widget-template.html'));
 
-    var WidgetView = require( 'js/components/widget/widget-view' );
-    var dateTimeService = require( '/resources/js/services/date-time-service.js' );
+    var WidgetView = require('js/components/widget/widget-view');
+    var dateTimeService = require('/resources/js/services/date-time-service.js');
 
     var Translator = require('translator');
     var translator = new Translator({
-        favoriteCategoriesStatistics: "Favorite categories statistics"
+        favoriteCategoriesStatistics: "Next day games"
         , matchesCount: "Matches count total"
         , betsCount: "Bets count"
         , noGames: "No games on date"
@@ -24,38 +24,38 @@ define(function (require) {
         events: {},
 
         initialize: function (options) {
-            this.listenTo( this.model, 'sync', this._renderStatistics );
+            this.listenTo(this.model, 'sync', this._renderStatistics);
             this.render();
         },
 
-		renderBody: function() {
-			this.model.fetch( { cache: false } );
-		},
+        renderBody: function () {
+            this.model.fetch({cache: false});
+        },
 
         getTitle: function () {
-            return translator.favoriteCategoriesStatistics + ':<br /><small>' + dateTimeService.formatDateFullDisplay( this.model.onDate ) + '</small>';
+            return translator.favoriteCategoriesStatistics + ':<br /><small>' + dateTimeService.formatDateFullDisplay(this.model.onDate) + '</small>';
         },
 
         getIcon: function () {
-			return 'fa fa-info-circle';
-		},
+            return 'fa fa-info-circle';
+        },
 
         getCustomMenuItems: function () {
-			return [];
-		},
+            return [];
+        },
 
         _renderStatistics: function () {
             var jmodel = this.model.toJSON();
             if (jmodel.categoryBetStatistics.length == 0) {
-                var noGames = "<span class='text-muted'>" + translator.noGames + ' <br /><strong>' + dateTimeService.formatDateFullDisplay( this.model.onDate ) + "</strong></span>";
+                var noGames = "<span class='text-muted'>" + translator.noGames + ' <br /><strong>' + dateTimeService.formatDateFullDisplay(this.model.onDate) + "</strong></span>";
                 this.setBody(noGames);
-                this.trigger( 'inner-view-rendered' );
+                this.trigger('inner-view-rendered');
                 return;
             }
             var data = _.extend({}, jmodel, {translator: translator});
             this.setBody(template(data));
 
-            this.trigger( 'inner-view-rendered' );
+            this.trigger('inner-view-rendered');
         }
     });
 });
